d25bf59da7bedae71065cfc5f36b5dcd9235f98f
Olivia Chapman <olivia.chapman@capycino-studios.com>
2025-05-30 11:34:49 +0100

    Added CRUD endpoints for customers

---
+++ LorryBoardAPI/Models/Reports/CustomerStats.cs
@@ -0,0 +1,9 @@
+﻿namespace LorryBoardAPI.Models.Reports;
+
+public class CustomerStats
+{
+    public Customer Customer { get; set; }
+    public int TotalOrders { get; set; }
+    public int TotalOnTime { get; set; }
+    public int TotalLate { get; set; }
+}
